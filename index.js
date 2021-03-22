635b3abd5686c3889c977e59c7102f5b7e4b45a7
drslaughterrex <drslaughterrex@gmail.com>
2021-03-22 13:18:58 -0500

    Create a CodeGrade submission

--- index.js
+++ index.js
@@ -34,11 +34,17 @@ processFirstItem(['foo','bar'], addStrings);
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
+  counter2 is referencing a global scoped count.
   
   2. Which of the two uses a closure? How can you tell?
+  counter1, because a closure is created by a function inside of a parent function.
   
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
+     counter 1 is good to create private variables and control side effects in the code.
+
+     counter2 is good when that isn't needed.
+    
 */
 
 // counter1 code
